57b6842b2bc64a28a6b1f7512f2cf5f43495fd73
jiaojian-chris <jiaojian_chris@163.com>
2017-06-29 16:24:10 +0800

    添加blog首页样式

--- school_yearbook/models.py
+++ school_yearbook/models.py
@@ -28,6 +28,9 @@ class User(models.Model):
     name = models.CharField('user name', max_length=75, help_text='user name.', unique=True)
     mobile = models.CharField('mobile', max_length=20, help_text='Mobile. No.')
     email = models.CharField('email', max_length=75, help_text='Email.')
+    wechat = models.CharField('WeChat', max_length=75, help_text='WeChat.')
+    address = models.CharField('address', max_length=255, blank=True, null=True,
+                               help_text=u'home address.')
     update_time = models.DateTimeField('update time', blank=True, null=True, default=datetime.datetime.now,
                                        help_text='update time.')
     is_valid = models.BooleanField(choices=IS_VALID, default=True)
@@ -36,3 +39,12 @@ class User(models.Model):
 
     class Meta:
         db_table = 'account_user'
+
+
+class Label(models.Model):
+    content = models.CharField('label content', max_length=36, help_text='label content')
+    create_time = models.DateTimeField('create_time', blank=True, null=True, default=datetime.datetime.now,
+                                       help_text='create_time.')
+    is_valid = models.BooleanField(choices=IS_VALID, default=True)
+
+
